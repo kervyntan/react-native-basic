7124096b17be9ded1bcf2d5671902fad0a599ec6
Kervyn <73232587+kervyntan@users.noreply.github.com>
2025-05-15 21:35:18 +0800

    feat: adding stylings

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View>
-      <Text> Home </Text>
+    <View style={styles.container}>
+      <Image style={styles.img} source={require("../assets/splash-icon.png")} />
+      <View>
+        <Text style={styles.title}> Home </Text>
+        <Text> Supermarket App </Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.title}> This is a test card. </Text>
+        <Text> Lalalala </Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+  },
+  img: {
+    marginVertical: 10,
+    width: 100,
+    height: 100
+  },
+});
